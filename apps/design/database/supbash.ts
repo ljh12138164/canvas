888bf6dd36cdb263e08361faadeebe5b3afd98fb
ljh12138164 <3479261099@qq.com>
2024-10-19 22:03:09 +0800

    feat:导出导入+数据库

--- apps/design/database/supbash.ts
+++ apps/design/database/supbash.ts
@@ -1,5 +1,25 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabase = createClient(supabaseUrl, supabaseKey);
-export { supabase };
+// import { auth } from "@/auth";
+// import { createClient } from "@supabase/supabase-js";
+// import { Session } from "next-auth";
+// type SessionSupabase = Session & { supabaseAccessToken: string };
+// async function supabaseClient() {
+//   // const session = await auth();
+//   // if (!session) {
+//   //   throw new Error("未登录");
+//   // }
+//   // const { supabaseAccessToken } = session as SessionSupabase;
+//   // const supabase = createClient(
+//   //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//   //   {
+//   //     global: {
+//   //       headers: {
+//   //         Authorization: `Bearer ${supabaseAccessToken}`,
+//   //       },
+//   //     },
+//   //   }
+//   // );
+//   // return supabase;
+// }
+
+// export { supabaseClient };
