9638ba677e5a433704ca3a66f2232c50862fe50c
ljh12138164 <3479261099@qq.com>
2024-11-10 20:52:58 +0800

    feat:test

--- apps/jebet/src/server/index.ts
+++ apps/jebet/src/server/index.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
-import type { App } from "../../../api/src/index";
+import type { App } from "api";
 
 export const client = hc<App>(import.meta.env.VITE_API_URL!);
