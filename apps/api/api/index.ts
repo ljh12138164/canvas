339a8c53589cdfe32cf89329ac88f07ce6971442
ljh12138164 <3479261099@qq.com>
2024-11-10 13:19:51 +0800

    feat：api规划

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -5,4 +5,9 @@ export const config = {
   runtime: "edge",
 };
 
-export default handle(app);
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+export const OPTIONS = handle(app);
