eef362ba87e8005547ca8bab998fbd51204a4e96
ljh12138164 <3479261099@qq.com>
2024-12-08 00:02:06 +0800

    feat:flods显示

--- apps/note/src/pages/workspace/workspaceItem.vue
+++ apps/note/src/pages/workspace/workspaceItem.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button';
-import Skeleton from '@/components/ui/skeleton/Skeleton.vue';
-import { useGetWorkspaceById } from '@/hooks/workspace';
-import useUser from '@/store/user';
-import { useQueryClient } from '@tanstack/vue-query';
-import { watch } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { Button } from "@/components/ui/button";
+import Skeleton from "@/components/ui/skeleton/Skeleton.vue";
+import { useGetWorkspaceById } from "@/hooks/workspace";
+import useUser from "@/store/user";
+import { useQueryClient } from "@tanstack/vue-query";
+import { watch } from "vue";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useRoute, useRouter } from "vue-router";
 const queryClient = useQueryClient();
 const token = useUser()?.userData?.session.access_token as string;
 
@@ -15,12 +23,15 @@ const { workspace, workspaceError, workspaceIsLoading, workspaceIsFetching } =
 watch(
   () => route.params.workspaceId,
   () => {
-    queryClient.invalidateQueries({ queryKey: ['workspaceItem'] });
-  },
+    queryClient.invalidateQueries({ queryKey: ["workspaceItem"] });
+  }
 );
 const router = useRouter();
 const handleManageCollaborators = () => {
   router.push(`/workspace/${route.params.workspaceId}/member`);
+};
+const handleGoFolder = (folderId: string) => {
+  router.push(`/workspace/${route.params.workspaceId}/folders/${folderId}`);
 };
 </script>
 <template>
@@ -29,9 +40,38 @@ const handleManageCollaborators = () => {
       <Skeleton class="w-full h-[300px] bg-[#d8d8d8] dark:bg-[#3a3a3a]" />
     </div>
     <div v-else-if="workspace">
-      <div>{{ workspace?.title }}</div>
-      <div>{{ workspace.profiles }}</div>
-      <Button @click="handleManageCollaborators">管理协作</Button>
+      <Table v-if="workspace.folders.length > 0">
+        <TableHeader>
+          <TableRow>
+            <TableHead class="w-[100px]"> 文件夹名字 </TableHead>
+            <TableHead>状态</TableHead>
+            <TableHead>方法</TableHead>
+            <TableHead class="text-right"> 文档数量 </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <div v-if="workspace.folders.length > 0">
+            <TableRow
+              v-for="folder in workspace.folders"
+              :key="folder.id"
+              @click="handleGoFolder(folder.id as string)"
+            >
+              <TableCell class="font-medium">
+                {{ folder.title }}
+              </TableCell>
+              <TableCell>{{ folder.inTrash ? "已删除" : "正常" }}</TableCell>
+              <TableCell>{{ folder.title }}</TableCell>
+              <TableCell class="text-right">
+                {{ folder.files.length }}
+              </TableCell>
+            </TableRow>
+          </div>
+        </TableBody>
+      </Table>
+      <div v-else>没有文件夹</div>
+      <div>
+        <Button @click="handleManageCollaborators">管理协作</Button>
+      </div>
     </div>
     <div v-else-if="workspaceError" class="text-red-500">
       <div>查询失败</div>
