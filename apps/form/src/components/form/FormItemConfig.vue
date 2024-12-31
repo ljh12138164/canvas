aabfe97bb796cf9072f664dcbe5a0a25e0b27059
ljh12138164 <3479261099@qq.com>
2025-01-01 00:47:26 +0800

    feat:基本类型完成

--- apps/form/src/components/form/FormItemConfig.vue
+++ apps/form/src/components/form/FormItemConfig.vue
@@ -2,13 +2,21 @@
 import type { CreateFormItem, FormType } from '@/types/form'
 import InputItem from './FormItem/InputItem.vue'
 import SelectItem from './FormItem/SelectItem.vue'
+import DateItem from './FormItem/DateItem.vue'
+import BigText from './FormItem/BigText.vue'
+import { DateValue } from '@internationalized/date'
+import OneClick from './FormItem/OneClick.vue'
+// @ts-ignore
+import Switch from './FormItem/switch.vue'
+import FileItem from './FormItem/FileItem.vue'
+
 defineProps<{
   id: string
   data: CreateFormItem | undefined
   updateList2: (
     id: string,
     type: FormType,
-    newValue: string | boolean | number | undefined | { name: string; id: string }[],
+    newValue: string | boolean | number | undefined | { name: string; id: string }[] | DateValue,
   ) => void
 }>()
 </script>
@@ -20,12 +28,20 @@ defineProps<{
     <div v-if="data.type === 'select'">
       <SelectItem :updateList2="updateList2" :id="id" :data="data" />
     </div>
-
-    <div v-if="data.type === 'checkbox'">
-      <!-- <CheckboxItem :updateList2="updateList2" :id="id" :data="data" /> -->
+    <div v-if="data.type === 'date'">
+      <DateItem :updateList2="updateList2" :id="id" :data="data" />
+    </div>
+    <div v-if="data.type === 'bigText'">
+      <BigText :updateList2="updateList2" :id="id" :data="data" />
+    </div>
+    <div v-if="data.type === 'slider'">
+      <Switch :updateList2="updateList2" :id="id" :data="data" />
+    </div>
+    <div v-if="data.type === 'file'">
+      <FileItem :updateList2="updateList2" :id="id" :data="data" />
     </div>
     <div v-if="data.type === 'radio'">
-      <!-- <RadioItem :updateList2="updateList2" :id="id" :data="data" /> -->
+      <OneClick :updateList2="updateList2" :id="id" :data="data" />
     </div>
   </div>
 </template>
--- apps/form/src/lib/form.ts
+++ apps/form/src/lib/form.ts
@@ -1,6 +1,15 @@
 import { z } from 'zod'
-import type { CreateFormItem, FormInput, Select } from '@/types/form'
+import type {
+  BigText,
+  CreateFormItem,
+  DatePicker,
+  FormInput,
+  Radio,
+  Select,
+  Slider,
+} from '@/types/form'
 import { Ref } from 'vue'
+import { Files } from '@/types/form'
 
 /**
  * ### zod的顺序说明 先用string和number等基础类型---required（min）必填---description标签---default默认---!required（optional）非必填
@@ -38,7 +47,9 @@ export const hasDescription = (schema: z.ZodType, value: string) => {
   return schema
 }
 
-z.string().min(2).default('123').describe('123').optional()
+//////////////////////////////////////// 下面是/😊😊
+//////////////////////////////////////// 基本类型😊😊
+
 /**
  * ###  zod的顺序说明 先用string和number等基础类型---required（min）必填---description标签---default默认---!required（optional）非必填
  * ## 下拉框的
@@ -66,6 +77,7 @@ export const selectZod = (schema: Select, fieldConfig: Ref<Record<string, any>>)
   fieldConfig.value = { ...fieldConfig.value, [schema.defaultTypeName]: obj }
   return zodSchema
 }
+
 /**
  * ###  zod的顺序说明 先用string和number等基础类型---required（min）必填---description标签---default默认---!required（optional）非必填
  * ## 生成input的zod校验
@@ -89,9 +101,12 @@ export const inputZod = function (schema: FormInput, fieldConfig: Ref<Record<str
     if (schema.description && !schema.hiddenLabel)
       zodSchema = hasDescription(zodSchema, schema.description)
     else obj.hideLabel = true
-
+    console.log(schema.defaultValue)
     // 默认值
-    zodSchema = zodSchema.default(String(schema.defaultValue))
+    if (schema.defaultValue) {
+      zodSchema = zodSchema.default(schema.defaultValue)
+      obj.inputProps.defaultValue = schema.defaultValue
+    }
     // 非必填
     if (!schema.isRequired) zodSchema = zodSchema.optional()
   }
@@ -123,6 +138,172 @@ export const inputZod = function (schema: FormInput, fieldConfig: Ref<Record<str
 }
 
 /**
+ * ###  zod的顺序说明 先用string和number等基础类型---required（min）必填---description标签---default默认---!required（optional）非必填
+ * ## 日期选择器
+ * @param schema 数据
+ * @param fieldConfig<Ref<Record<string, any>>>  配置
+ * @returns zod校验
+ */
+export const dateZod = (schema: DatePicker, fieldConfig: Ref<Record<string, any>>) => {
+  let obj: Record<string, any> = {
+    inputProps: {},
+  }
+  let zodSchema
+  zodSchema = z.coerce.date()
+  if (schema.description && !schema.hiddenLabel) {
+    zodSchema = hasDescription(zodSchema, schema.description)
+  } else {
+    obj.hideLabel = true
+  }
+
+  obj.inputProps.description = 'description'
+  // 非必填
+  if (!schema.isRequired) zodSchema = zodSchema.optional()
+  // 默认值
+  // if (schema.defaultValue) {
+  //   zodSchema = zodSchema.default(new Date(schema.defaultValue as string).toISOString())
+  //   obj.inputProps.defaultValue = schema.defaultValue
+  // }
+  // 占位符
+  obj = genAutoFormPlaceHolder(obj, schema.placeholder)
+  // 检查inputProps是否存在
+  obj = checkInputProps(obj)
+  console.log(obj)
+  fieldConfig.value = { ...fieldConfig.value, [schema.defaultTypeName]: obj }
+  return zodSchema
+}
+
+/** 
+  
+* ### 大文本
+ * @param schema 数据
+ * @param fieldConfig<Ref<Record<string, any>>>  配置
+ * @returns zod校验
+ */
+export const bigTextZod = (schema: BigText, fieldConfig: Ref<Record<string, any>>) => {
+  let obj: Record<string, any> = {
+    inputProps: {},
+    component: 'textarea',
+  }
+  let zodSchema
+  zodSchema = z.string()
+  // 必填
+  if (schema.isRequired) zodSchema = zodSchema.min(1, { message: '必填' })
+  // 标签
+  if (schema.description && !schema.hiddenLabel)
+    zodSchema = hasDescription(zodSchema, schema.description)
+  else obj.hideLabel = true
+
+  // 默认值
+  zodSchema = zodSchema.default(String(schema.defaultValue))
+  // 非必填
+  if (!schema.isRequired) zodSchema = zodSchema.optional()
+  // 占位符
+  obj = genAutoFormPlaceHolder(obj, schema.placeholder)
+  // 检查inputProps是否存在
+  obj = checkInputProps(obj)
+  // 生成autoForm的配置
+  fieldConfig.value = { ...fieldConfig.value, [schema.defaultTypeName]: obj }
+  return zodSchema
+}
+
+/**
+ * ## 滑动按钮
+ * @param schema 数据
+ * @param fieldConfig<Ref<Record<string, any>>>  配置
+ * @returns zod校验
+ */
+export const sliderZod = (schema: Slider, fieldConfig: Ref<Record<string, any>>) => {
+  let obj: Record<string, any> = {
+    inputProps: {},
+    component: 'switch',
+  }
+  let zodSchema
+  zodSchema = z.boolean()
+  // 必填
+  if (schema.isRequired)
+    zodSchema = zodSchema.refine((value) => value, {
+      message: '必填',
+      path: [schema.defaultTypeName],
+    })
+  // 标签
+  if (schema.description && !schema.hiddenLabel)
+    zodSchema = hasDescription(zodSchema, schema.description)
+  else obj.hideLabel = true
+  // 默认值
+  zodSchema = zodSchema.default(Boolean(schema.defaultValue))
+  // 非必填
+  if (!schema.isRequired) zodSchema = zodSchema.optional()
+  // 占位符
+  obj = genAutoFormPlaceHolder(obj, schema.placeholder)
+  // 检查inputProps是否存在
+  obj = checkInputProps(obj)
+  // 生成autoForm的配置
+  fieldConfig.value = { ...fieldConfig.value, [schema.defaultTypeName]: obj }
+  return zodSchema
+}
+
+/**
+ * ###  zod的顺序说明 先用string和number等基础类型---required（min）必填---description标签---default默认---!required（optional）非必填
+ * ## 下拉框的
+ * @param schema 数据
+ * @returns zod
+ */
+export const radioZod = (schema: Radio, fieldConfig: Ref<Record<string, any>>) => {
+  let obj: Record<string, any> = {
+    inputProps: {},
+    component: 'radio',
+  }
+  let zodSchema
+  zodSchema = z.enum(schema.options.map((item) => item.name) as [string, ...string[]])
+  if (schema.description && !schema.hiddenLabel) {
+    zodSchema = hasDescription(zodSchema, schema.description)
+  } else {
+    obj.hideLabel = true
+  }
+
+  // 非必填
+  if (!schema.isRequired) zodSchema = zodSchema.optional()
+  // 占位符
+  obj = genAutoFormPlaceHolder(obj, schema.placeholder)
+  // 检查inputProps是否存在
+  obj = checkInputProps(obj)
+  fieldConfig.value = { ...fieldConfig.value, [schema.defaultTypeName]: obj }
+  return zodSchema
+}
+/**
+ * 文件
+ * @param schema 数据
+ * @param fieldConfig<Ref<Record<string, any>>>  配置
+ * @returns zod校验
+ */
+export const fileZod = (schema: Files, fieldConfig: Ref<Record<string, any>>) => {
+  let obj: Record<string, any> = {
+    inputProps: {},
+    component: 'file',
+  }
+  let zodSchema
+  zodSchema = z.string()
+  // 必填
+  if (schema.isRequired) zodSchema = zodSchema.min(1, { message: '必填' })
+  // 标签
+  if (schema.description && !schema.hiddenLabel)
+    zodSchema = hasDescription(zodSchema, schema.description)
+  else obj.hideLabel = true
+
+  // 默认值
+  zodSchema = zodSchema.default(String(schema.defaultValue))
+  // 非必填
+  if (!schema.isRequired) zodSchema = zodSchema.optional()
+  // 占位符
+  obj = genAutoFormPlaceHolder(obj, schema.placeholder)
+  // 检查inputProps是否存在
+  obj = checkInputProps(obj)
+  // 生成autoForm的配置
+  fieldConfig.value = { ...fieldConfig.value, [schema.defaultTypeName]: obj }
+  return zodSchema
+}
+/**
  * ## 生成zod校验
  * @param schema 数据
  * @param fieldConfig<Ref<Record<string, any>>>  配置
@@ -134,22 +315,37 @@ export function getZodSchema(schema: CreateFormItem, fieldConfig: Ref<Record<str
     const zodSchema = inputZod(schema, fieldConfig)
     return z.object({ [schema.defaultTypeName]: zodSchema as z.ZodType })
   }
-  // 复选框
-  if (schema.type === 'checkbox') return z.object({ [schema.name]: z.boolean() })
+  // 文件
+  if (schema.type === 'file') {
+    const zodSchema = fileZod(schema, fieldConfig)
+    return z.object({ [schema.defaultTypeName]: zodSchema as z.ZodType })
+  }
   // 单选框
-  if (schema.type === 'radio') return z.object({ [schema.name]: z.string() })
+  if (schema.type === 'radio') {
+    const zodSchema = radioZod(schema, fieldConfig)
+    return z.object({ [schema.defaultTypeName]: zodSchema as z.ZodType })
+  }
   // 下拉框
   if (schema.type === 'select') {
     const zodSchema = selectZod(schema, fieldConfig)
     return z.object({ [schema.defaultTypeName]: zodSchema as z.ZodType })
   }
-  // if (schema.type === 'textarea') return z.string({ [schema.id]: z.string() })
-  // if (schema.type === 'date') return z.string({ [schema.id]: z.string() })
-  // if (schema.type === 'time') return z.string({ [schema.id]: z.string() })
-  // if (schema.type === 'datetime') return z.string({ [schema.id]: z.string() })
-  // if (schema.type === 'datetime-local') return z.string({ [schema.id]: z.string() })
-  // if (schema.type === 'email') return z.string({ [schema.id]: z.string() })
-  // if (schema.type === 'number') return z.string({ [schema.id]: z.number() })
+  // 日期
+  if (schema.type === 'date') {
+    const zodSchema = dateZod(schema, fieldConfig)
+    return z.object({ [schema.defaultTypeName]: zodSchema as z.ZodType })
+  }
+  // 大文本
+  if (schema.type === 'bigText') {
+    const zodSchema = bigTextZod(schema, fieldConfig)
+    return z.object({ [schema.defaultTypeName]: zodSchema as z.ZodType })
+  }
+  // 滑动按钮
+  if (schema.type === 'slider') {
+    const zodSchema = sliderZod(schema, fieldConfig)
+    return z.object({ [schema.description || schema.defaultTypeName]: zodSchema as z.ZodType })
+  }
+
   return null
 }
 
