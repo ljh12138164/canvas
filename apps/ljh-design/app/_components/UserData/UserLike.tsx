0c9135931c4a7801bc70e33696e859d45cebd441
ljh12138164 <3479261099@qq.com>
2025-01-25 23:54:06 +0800

    feat:(design)点赞和收藏的完成

---
+++ apps/ljh-design/app/_components/UserData/UserLike.tsx
@@ -0,0 +1,41 @@
+import type { UserResponseType } from '@/app/_hook/query/useUser';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/Avatar';
+import { Loading } from './Loading';
+
+interface UserLike {
+  data: UserResponseType | undefined;
+  loading: boolean;
+}
+
+export const UserLike = ({ data, loading }: UserLike) => {
+  if (loading) return <Loading />;
+  if (!data?.length)
+    return (
+      <section className="flex justify-center items-center h-screen">
+        <p className="text-2xl text-gray-500">暂无数据</p>
+      </section>
+    );
+
+  return (
+    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 space-y-5 p-4">
+      {data.map((item) => (
+        <div
+          key={item.show.id}
+          className="group h-[150px] relative overflow-hidden rounded-lg transition-all hover:scale-105"
+        >
+          <Link href={`/board/detail/${item.show.id}`}>
+            <Avatar>
+              <AvatarImage src={item.show.profiles.image} />
+              <AvatarFallback>{item.show.profiles.name.slice(0, 2)}</AvatarFallback>
+            </Avatar>
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-transparent p-4">
+              <h3 className="text-sm font-medium text-white line-clamp-2">{item.show.title}</h3>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </section>
+  );
+};
