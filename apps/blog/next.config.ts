f24ecd41be5b8e88cd54943534849cc50daab1e3
ljh12138164 <3479261099@qq.com>
2025-01-26 23:29:18 +0800

    feat(基建)：博客图片优化

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -1,6 +1,8 @@
 import nextra from 'nextra';
 
-const withNextra = nextra({});
+const withNextra = nextra({
+  staticImage: true,
+});
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
