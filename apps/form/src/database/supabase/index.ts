48973cf89cc7b73170d5ae756f55556ea71c69eb
ljh12138164 <3479261099@qq.com>
2025-01-31 21:03:10 +0800

    fix:(form)更新配置文件

--- apps/form/src/database/supabase/index.ts
+++ apps/form/src/database/supabase/index.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_FORM_URL!;
-const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_FORM_KEY!;
+const supabaseUrl =
+  import.meta.env.VITE_PUBLIC_SUPABASE_FORM_URL! || 'https://spvppoqewfwqyzlsmtru.supabase.co';
+const supabaseKey =
+  import.meta.env.VITE_PUBLIC_SUPABASE_FORM_KEY! ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdnBwb3Fld2Z3cXl6bHNtdHJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4ODA4MzgsImV4cCI6MjA0OTQ1NjgzOH0.HzXRR0AG0WYAyDjH3Df4pbW7b2nR2tOcRoDr0xd3eF0';
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL or key is not set');
 }
