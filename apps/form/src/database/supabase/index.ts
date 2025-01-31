ad8dfec4941e23fcd67f32499fc1b687e1b2a600
ljh12138164 <3479261099@qq.com>
2025-01-31 20:58:14 +0800

    fix:(form)更新配置文件

--- apps/form/src/database/supabase/index.ts
+++ apps/form/src/database/supabase/index.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY!;
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_FORM_URL!;
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_FORM_KEY!;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL or key is not set');
 }
--- apps/form/vite.config.ts
+++ apps/form/vite.config.ts
@@ -57,18 +57,18 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            // 让每个插件都打包成独立的文件
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: (id) => {
+  //         if (id.includes('node_modules')) {
+  //           // 让每个插件都打包成独立的文件
+  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
