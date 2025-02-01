a2679866c8c56c0a0a43a7261f3a745e8f74cf35
ljh12138164 <3479261099@qq.com>
2025-02-02 02:04:46 +0800

    fix:(jebet)清除热更新

--- apps/jebet/vite.config.ts
+++ apps/jebet/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // fastRefresh: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
