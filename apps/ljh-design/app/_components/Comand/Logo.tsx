37626148cc201310ffed92746bc7b9702cd3923c
ljh12138164 <3479261099@qq.com>
2025-02-10 21:40:26 +0800

    fix(design)部署错误

--- apps/ljh-design/app/_components/Comand/Logo.tsx
+++ apps/ljh-design/app/_components/Comand/Logo.tsx
@@ -1,11 +1,17 @@
-import logo from '@/app/public/logoImage.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Logo = ({ to }: { to: string }) => {
   return (
     <Link href={to || '/'} className="h-12 w-12 cursor-pointer flex items-center justify-center">
-      <Image width={48} quality={75} height={48} src={logo} alt="logo" priority={true} />
+      <Image
+        width={48}
+        quality={75}
+        height={48}
+        src="https://osdawghfaoyysblfsexp.supabase.co/storage/v1/object/public/ljh-design-ui//logoImage.jpg"
+        alt="logo"
+        priority={true}
+      />
     </Link>
   );
 };
