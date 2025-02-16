3bebdd5140d3e7a27c8b6c9671115a182da6b1d6
ljh12138164 <3479261099@qq.com>
2025-02-16 19:01:15 +0800

    feat(design)动态生成seo标题

--- apps/ljh-design/app/_database/index.ts
+++ apps/ljh-design/app/_database/index.ts
@@ -1,6 +1,7 @@
 import type { App } from 'api';
 import { hc } from 'hono/client';
 // 使用api的类型
+// 解析App
 export const client = hc<App>('https://www.ljhboard.cn/').api.design;
 // export const client = hc<App>('http://localhost:8000/').api.design;
 
--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -1,9 +1,58 @@
+import { AutoForm } from '@autoform/ant';
+import { ZodProvider } from '@autoform/zod';
 // import { useEffect, useState } from "react";
 // import type { App } from "api";
 // import { hc } from "hono/client";
+import { z } from 'zod';
+enum Sports {
+  Football = 'Football/Soccer',
+  Basketball = 'Basketball',
+  Baseball = 'Baseball',
+  Hockey = 'Hockey (Ice)',
+  None = "I don't like sports",
+}
 
 // const client = hc<App>("https://www.ljhboard.cn/").api.ai;
-
+const zodSchemaProvider = new ZodProvider(
+  z.object({
+    password: z
+      .string({
+        required_error: 'Password is required.',
+      })
+      .describe('Your secure password')
+      .min(8, {
+        message: 'Password must be at least 8 characters.',
+      }),
+    favouriteNumber: z.coerce
+      .number({
+        invalid_type_error: 'Favourite number must be a number.',
+      })
+      .min(1, {
+        message: 'Favourite number must be at least 1.',
+      })
+      .max(10, {
+        message: 'Favourite number must be at most 10.',
+      })
+      .default(1)
+      .optional(),
+    acceptTerms: z
+      .boolean()
+      .describe('Accept terms and conditions.')
+      .refine((value) => value, {
+        message: 'You must accept the terms and conditions.',
+        path: ['acceptTerms'],
+      }),
+    sendMeMails: z.boolean().optional(),
+    birthday: z.coerce.date({ message: 'aaa' }).optional(),
+    color: z.enum(['red', 'green', 'blue']).optional(),
+    // Another enum example
+    marshmallows: z
+      .enum(['not many', 'a few', 'a lot', 'too many'])
+      .describe('How many marshmallows fit in your mouth?'),
+    // Native enum example
+    sports: z.nativeEnum(Sports).describe('What is your favourite sport?'),
+  }),
+);
 function App() {
   // const [image, setImage] = useState<File | null>(null);
   // useEffect(() => {
@@ -39,6 +88,14 @@ function App() {
           e.target.value = "";
         }}
       /> */}
+      <AutoForm
+        onSubmit={(data) => {
+          // console.log(data);
+        }}
+        schema={zodSchemaProvider}
+      >
+        <button type="submit">提交</button>
+      </AutoForm>
     </section>
   );
 }
